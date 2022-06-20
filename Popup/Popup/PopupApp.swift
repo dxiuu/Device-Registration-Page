6557d4b657b14c63390ea720c7386aa6643408cf
David Xiu <62933781+dxiuu@users.noreply.github.com>
2022-06-20 14:15:08 -0700

    Add files via upload

---
+++ Popup/Popup/PopupApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopupApp.swift
+//  Popup
+//
+//  Created by dxiu on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PopupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
